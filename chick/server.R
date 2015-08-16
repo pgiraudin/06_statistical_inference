3717ebf56f2de786d8bdb34a5f8b9f12488b7428
pierre-francois giraudin <pgiraudin@hotmail.com>
2015-08-16 15:16:03 +0200

    v2
    V2

--- chick/server.R
+++ chick/server.R
@@ -13,21 +13,30 @@ shinyServer(function(input, output) {
       
       #case of spaghetti graphic
       if (input$Graphic == 1){
-        p<-ggplot(data = dataSelection, aes(x = Time, y = weight, group = Chick, colour = Diet));
+        p <- ggplot(data = dataSelection, aes(x = Time, y = weight, group = Chick, colour = Diet));
         p <- p + geom_line(); 
         p <- p + facet_grid(Diet ~ .) ;        
       
       #case of boxplot graphic
       }else if (input$Graphic == 2){
-        p<-ggplot(data = dataSelection, aes(x = Time, y = weight, group = Time,colour = Diet))
+        p <- ggplot(data = dataSelection, aes(x = Time, y = weight, group = Time,colour = Diet))
         p <- p + geom_boxplot(); 
         p <- p + facet_wrap(~Diet, ncol=2);        
       }
       print(p);
     }
-    else{
-      print("No data to display")
-    }
-    
   })
+  
+  output$Documentation <- renderText({
+    t1 <- h2("Documentation");
+    t2 <- "This application aims to create graphics for the exploration of the \"ChickWeight\" data."
+    t3 <- "The sidepanel is composed of two elements : "
+    t4 <- "- a list of checkbox that represent the different diets for chicks"
+    t5 <- "- two radio buttons that represent the type of graphic to render whit the selected datas."
+    t6 <- "<br>"
+    t7 <- "To display the datas, select the Diet checkbox that you want to display, then select a type of graphic, then click on the submit button."
+    t8 <- "results are show in the main panel"
+    print(paste(t1, t2, t3, t4, t5, t6, t7, t8, sep = "<br>"))
+  })
+  
 })
--- chick/ui.R
+++ chick/ui.R
@@ -3,23 +3,30 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       checkboxGroupInput("Diet", 
-                  label = h3("Select Diet"), 
-                  choices = list("Diet 1" = 1,
-                                 "Diet 2" = 2,
-                                 "Diet 3" = 3,
-                                 "Diet 4" = 4),
-                            selected =c(1,2,3,4)
-                  ),
+                         label = h3("Select Diet"), 
+                         choices = list("Diet 1" = 1,
+                                        "Diet 2" = 2,
+                                        "Diet 3" = 3,
+                                        "Diet 4" = 4),
+                         selected =c(1,2,3,4)
+      ),
       radioButtons("Graphic", label = h3("Graphic type"),
                    choices = list("spaghetti" = 1, 
                                   "boxplot" = 2)
-                             ,selected = 1
-                  ),
+                   ,selected = 1
+      ),
       submitButton("Submit")
-              
+      
     ),
-    mainPanel(label =h3("main panel"),
-              plotOutput("chickPlot")
+    mainPanel(
+      tabsetPanel(
+        tabPanel("main panel",
+                 plotOutput("chickPlot")
+        ), 
+        tabPanel("Documentation",
+                 htmlOutput("Documentation")
+        )              
+      )
     )
   )
 ))
